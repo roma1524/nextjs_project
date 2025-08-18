9ac21a25a949f4a94872eb8c42e9d2c935fd967f
roma1524 <kulikr.js@mail.ru>
2025-08-18 23:02:54 +0300

    create component of characters

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'rickandmortyapi.com',
+        pathname: '/api/character/avatar/**'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
