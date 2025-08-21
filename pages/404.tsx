68585dfbc40b3a9fff673194f07fc5f0a498b922
roma1524 <kulikr.js@mail.ru>
2025-08-21 23:23:54 +0300

    someFix: 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import {getLayout} from "@/components/Layout/Layout";
+import Link from "next/link";
 
 const NotFound = () => {
     return (
+        <>
         <div>404 Not Found</div>
+        <Link href={'/'}/>
+        </>
     );
 }
 
